366b8ad9f0ee0c97dd2187a5c75b497e498539da
allison <allison.dixon@ssfs.org>
2019-12-05 12:50:15 -0500

    worked on unit 9 project

---
+++ unit9_option1.py
@@ -0,0 +1,21 @@
+# created by Allison Dixon
+# last modified on December 5, 2019
+# unit 9 option 1: this program creates a game of Compare (a small game of War) between the user and the dealer
+
+import card
+import deck
+
+user_cards = []
+dealer_cards = []
+shuffled = deck.Deck
+
+def deal_card():
+
+
+
+
+
+
+
+
+
